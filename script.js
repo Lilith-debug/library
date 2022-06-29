21c3f3d3dcb4ccbfb61668809f2a71ae073bb46e
lilith-debug <lilith.nil@mailfence.com>
2022-06-29 15:34:07 -0300

    Add book adding functionality

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    this.read = read === true ? "Yes" : "No"
+    
 }
 
 function addBookToLibrary(newBook) {
@@ -13,19 +14,34 @@ function addBookToLibrary(newBook) {
 
 function displayBooks() {
     for (let book in myLibrary) {
-        const newRow = document.createElement("tr");
+        if (!document.getElementById(`${myLibrary[book].title}`)) {    
+            const newRow = document.createElement("tr");
+            newRow.setAttribute("id", `${myLibrary[book].title}`)
 
-        for (let info in myLibrary[book]) {
-            const newCell = document.createElement("td");
-            newCell.textContent = `${(myLibrary[book])[info]}`;
-            newRow.appendChild(newCell);
+            for (let info in myLibrary[book]) {
+                const newCell = document.createElement("td");
+                newCell.textContent = `${(myLibrary[book])[info]}`;
+                newRow.appendChild(newCell);
+            }
+
+            tableBody.appendChild(newRow);
         }
-
-        tableBody.appendChild(newRow);
     }
 }
 
 const tableBody = document.querySelector("tbody");
+
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const bookRead = document.querySelector("#read");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", () => {
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
+    addBookToLibrary(newBook);
+    displayBooks();
+})
 
 let theHobbit = Object.create(Book.prototype);
 theHobbit.title = "The Hobbit";
